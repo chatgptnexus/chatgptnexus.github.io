c61b47be7e3c9035660bbed656c5d0d83b7b168e
jason <chatgptnexus@protonmail.com>
2024-01-22 19:10:54 +0900

    Updates

---
+++ serviceWorkerRegister.js
@@ -0,0 +1,10 @@
+if ('serviceWorker' in navigator) {
+  console.log('location: ', window.location)
+  window.addEventListener('load', function () {
+    navigator.serviceWorker.register(window.location.pathname + "serviceWorker.js").then(function (registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function (err) {
+      console.error('ServiceWorker registration failed: ', err);
+    });
+  });
+}
